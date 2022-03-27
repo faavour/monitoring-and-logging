ad87151b2bf9d02e2635da8afc1af41e8be4d862
Favour John Udoh <favourudoh@Udemes-MacBook-Pro.local>
2022-03-27 03:11:24 +0100

    Created aws instances with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,47 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.27"
     }
   }
+
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  region = "us-east-1"
+  profile = "default"
+  region  = "us-west-2"
 }
 
-resource "aws_instance" "test-instance" {}
+resource "aws_instance" "monitoring" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Monitoring Server"
+
+  }
+
+}
+
+resource "aws_instance" "logging" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Logging Server"
+
+  }
+
+}
+
+resource "aws_instance" "zeigeist" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Zeigeist Server"
+
+  }
+
+}
